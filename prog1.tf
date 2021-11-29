24c2ce9ecd2db9f0062f63b74b4d735d28588ca6
vidhya3b <91215867+vidhya3b@users.noreply.github.com>
2021-11-29 08:50:48 +0530

    adding s3

---
+++ prog1.tf
@@ -0,0 +1,18 @@
+terraform {
+    required_version = "~> 1.0.0"
+    required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+     }
+    }
+   }
+
+provider "aws" {
+	profile = "default"
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "big-basket" {
+	bucket = "my-big-basket-class1"
+}
